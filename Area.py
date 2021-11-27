a4c3127ea2cf7f511b8f582ca7ee04579054f81e
YJajodia27 <91831488+YJajodia27@users.noreply.github.com>
2021-11-27 17:41:39 +0530

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,6 @@
+#write a Python program which accepts the radius of a circle from the user and computes area.
+print("To find out area of a circle:")
+r=eval(input("Please enter the radius"))
+area=3.17*r*r
+print("Area of the circle=",area)
+
---
+++ fileextension.py
@@ -0,0 +1,4 @@
+#write a Python program to accept a filename from the user and print the extension of that.
+fn= input("Enter Filename: ")
+f = fn.split(".")
+print ("Extension of the file is : " + f[-1])
